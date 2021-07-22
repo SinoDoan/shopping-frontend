22a8eba0edc4a85d58c80ededb095e6f45eca215
Trung Doan <dctrung95@gmail.com>
2021-07-22 21:06:42 +0700

    addProject

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Product;
+use App\Slider;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $sliders = Slider::latest()->get();
+        $categories = Category::where('parent_id', 0)->get();
+        $products = Product::latest()->take(6)->get();
+        return view('pages.home', compact('sliders', 'categories', 'products'));
+    }
+}
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    @yield('title')
+    <link href="{{asset('frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/main.css')}}" rel="stylesheet">
+    @yield('css')
+</head>
+<body>
+    @include('components.header')
+    @yield('content')
+    @include('components.footer')
+    <script src="{{asset('frontend/js/jquery.js')}}"></script>
+    <script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('frontend/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{asset('frontend/js/price-range.js')}}"></script>
+    <script src="{{asset('frontend/js/jquery.prettyPhoto.js')}}"></script>
+    <script src="{{asset('frontend/js/main.js')}}"></script>
+    @yield('js')
+</body>
+</html>
